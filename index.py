8d0c1a5384a6973ae4470f58dc57561c27842aca
samridhikansal <kansal.samridhi@gmail.com>
2024-12-24 22:29:08 -0700

    First commit

---
+++ index.py
@@ -0,0 +1,16 @@
+
+print("Press 1 for adding a student \n Press 2 for deleting a student \n Press 3 for updating the student \n Press4 for viewing the deatils of the student")
+choice = int(input("\nEnter your Choice Now:"))
+if choice == 1:
+    print("You have selected to add the student")
+elif choice ==2:
+    print("You have selected to delete the student")
+elif choice ==3:
+    print("You have selected to update the student")
+elif choice ==4 :
+    print ("You have selected to view the student details")
+else:
+    print("You have selected the wrong choice. Try again")
+
+student = []
+
